fddb7be5931a30295b04db73201e5c8e65b6c3c0
RJ <esquiviasrj@gmail.com>
2021-08-28 10:35:29 -0700

    Add ErrorMessage into Formik login Form

--- reactclient/src/components/Users/LoginForm.tsx
+++ reactclient/src/components/Users/LoginForm.tsx
@@ -1,6 +1,6 @@
-import { Form, Formik } from "formik";
+import { ErrorMessage, Form, Formik } from "formik";
 import { observer } from "mobx-react-lite";
-import { Button, Container, Grid } from "semantic-ui-react";
+import { Button, Container, Grid, Label } from "semantic-ui-react";
 import { useStore } from "../../stores/store";
 import TextInput from "../TextInput";
 
@@ -15,13 +15,17 @@ export default observer(function LoginForm() {
                 </Grid.Column>
                 <Grid.Column>
                     <Formik 
-                    initialValues={{email: '', password: ''}}
-                    onSubmit={values => userStore.login(values)}
+                    initialValues={{email: '', password: '', error: null}}
+                    onSubmit={(values, {setErrors}) => userStore.login(values).catch(error => 
+                        setErrors({error: 'Invalid email or password'}))}
                     >
-                        {({handleSubmit, isSubmitting}) => (
+                        {({handleSubmit, isSubmitting, errors}) => (
                             <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
                                 <TextInput name='email' placeholder='Email' />
                                 <TextInput name='password' placeholder='Password' type='password'/>
+                                <ErrorMessage 
+                                    name='error' render={() => <Label style={{marginBottom: 10}} basic color='red' content={errors.error} />}    
+                                />
                                 <Button loading={isSubmitting} positive content='Login' type='submit' fluid />
                             </Form>
                         )}
